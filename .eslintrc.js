f032316b27d49eaa2e7d793287774d499eaac770
bellDev-code <sumaoo20@naver.com>
2022-11-15 17:59:56 +0900

    Add eslint parser, endOfLine

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: 'babel-parser',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
