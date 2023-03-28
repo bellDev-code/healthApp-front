ea49f13ed360ab9cded88e2127f197ffd7762a24
bellDev-code <sumaoo20@naver.com>
2023-03-28 23:22:23 +0900

    apollo reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import LoggedOutNav from './navigators/LoggedOutNav';
-import {ApolloProvider} from '@apollo/client';
-import client from '../apollo';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <NavigationContainer>
-        <LoggedOutNav />
-      </NavigationContainer>
-    </ApolloProvider>
+    <NavigationContainer>
+      <LoggedOutNav />
+    </NavigationContainer>
   );
 }
 
