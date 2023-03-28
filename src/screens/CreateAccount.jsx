db0944acc6c258b5cac1da8a17546634bc6d6fde
bellDev-code <sumaoo20@naver.com>
2023-03-28 15:30:52 +0900

    CreateAccount & Login React-hook-form v7

--- src/screens/CreateAccount.jsx
+++ src/screens/CreateAccount.jsx
@@ -2,9 +2,10 @@ import React, {useRef} from 'react';
 import AuthLayOut from '../components/auth/AuthLayOut';
 import AuthButton from '../components/auth/AuthButton';
 import {TextInput} from '../components/auth/AuthShared';
-import {KeyboardAvoidingView, Platform} from 'react-native';
+import {Controller, useForm} from 'react-hook-form';
 
 export default function CreateAccount() {
+  const {control, handleSubmit} = useForm();
   const lastNameRef = useRef();
   const usernameRef = useRef();
   const emailRef = useRef();
@@ -20,43 +21,98 @@ export default function CreateAccount() {
 
   return (
     <AuthLayOut>
-      <TextInput
-        placeholder="First Name"
-        returnKeyType="next"
-        placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
-        onSubmitEditing={() => onNext(lastNameRef)}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        name="firstName"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            placeholder="firstName"
+            returnKeyType="next"
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+            onSubmitEditing={() => onNext(lastNameRef)}
+          />
+        )}
       />
-      <TextInput
-        ref={lastNameRef}
-        placeholder="Last Name"
-        returnKeyType="next"
-        placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
-        onSubmitEditing={() => onNext(usernameRef)}
+      <Controller
+        control={control}
+        name="lastName"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            ref={lastNameRef}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            placeholder="lastName"
+            returnKeyType="next"
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+            onSubmitEditing={() => onNext(usernameRef)}
+          />
+        )}
       />
-      <TextInput
-        ref={usernameRef}
-        placeholder="Username"
-        returnKeyType="next"
-        placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
-        onSubmitEditing={() => onNext(emailRef)}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        name="username"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            ref={usernameRef}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            placeholder="username"
+            returnKeyType="next"
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+            onSubmitEditing={() => onNext(emailRef)}
+          />
+        )}
       />
-      <TextInput
-        ref={emailRef}
-        placeholder="Email"
-        keyboardType="email-address"
-        returnKeyType="next"
-        placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
-        onSubmitEditing={() => onNext(passwordRef)}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        name="email"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            ref={emailRef}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            placeholder="email"
+            returnKeyType="next"
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+            onSubmitEditing={() => onNext(passwordRef)}
+          />
+        )}
       />
-      <TextInput
-        ref={passwordRef}
-        placeholder="Password"
-        secureTextEntry
-        returnKeyType="done"
-        placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
-        onSubmitEditing={onDone}
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        name="password"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            ref={passwordRef}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            placeholder="password"
+            returnKeyType="next"
+            secureTextEntry
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+          />
+        )}
       />
-      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+      <AuthButton text="Create Account" loading onPress={() => null} />
     </AuthLayOut>
   );
 }
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,66 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
+import {useRef} from 'react';
+import {Controller, useForm} from 'react-hook-form';
+import AuthButton from '../components/auth/AuthButton';
+import AuthLayOut from '../components/auth/AuthLayOut';
+import {TextInput} from '../components/auth/AuthShared';
 
-export default function Login({navigation}) {
+export default function Login() {
+  const {control, handleSubmit, formState} = useForm();
+
+  const passwordRef = useRef();
+  const onNext = nextOn => {
+    nextOn?.current?.focus();
+  };
+  const onValid = data => {
+    console.log(data);
+  };
+
   return (
-    <View>
-      <Text>Login page</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
-        <Text>go to the CreateAccount</Text>
-      </TouchableOpacity>
-    </View>
+    <AuthLayOut>
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        name="username"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            placeholder="username"
+            returnKeyType="next"
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+            onSubmitEditing={() => onNext(passwordRef)}
+          />
+        )}
+      />
+      <Controller
+        control={control}
+        rules={{
+          required: true,
+        }}
+        name="password"
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            placeholder="password"
+            ref={passwordRef}
+            onChangeText={onChange}
+            onBlur={onBlur}
+            value={value}
+            secureTextEntry
+            returnKeyType="Done"
+            lastOne={true}
+            placeholderTextColor={'rgba(255, 255, 255, 0.6)'}
+          />
+        )}
+      />
+      <AuthButton
+        text="Log in"
+        disabled={true}
+        onPress={handleSubmit(onValid)}
+      />
+    </AuthLayOut>
   );
 }
