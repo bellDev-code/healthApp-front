157dec25333bdeea638ee9248a1182eb03b5ba37
bellDev-code <sumaoo20@naver.com>
2023-04-22 22:02:51 +0900

    react-native time-picker

---
+++ src/screens/TimePicker.jsx
@@ -0,0 +1,10 @@
+import react from 'react';
+import {View, Text} from 'react-native';
+
+export default function TimePicker() {
+  return (
+    <View>
+      <Text>Time Register</Text>
+    </View>
+  );
+}
